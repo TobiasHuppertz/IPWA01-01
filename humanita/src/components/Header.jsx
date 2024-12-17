6775c96f62567faf6edf4510697f431e357ff4fd
Tobias Huppertz <tobias.huppertz@iu-study.org>
2024-12-17 11:17:25 +0100

    Added Toggle Menu to Navbar

--- humanita/src/components/Header.jsx
+++ humanita/src/components/Header.jsx
@@ -1,12 +1,19 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import { navbar } from '../constants';
 
 export default function Navbar() {
+
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="bg-white w-full z-20 top-0 start-0 border-b border-gray-200">
       <div className="flex flex-wrap items-center justify-between mx-auto p-4">
         <Link to="../" className="flex items-center space-x-3">
-          <img src="../src/assets/humanita_logo.svg" alt="Logo" className="h-10" />
+          <img src="../src/assets/humanita_logo.svg" alt="Logo Humanita" className="h-10" />
           <span className="self-center text-2xl font-semibold whitespace-nowrap">Huminata</span>
         </Link>
         <div className="flex md:order-2 space-x-3 md:space-x-0">
@@ -16,8 +23,13 @@ export default function Navbar() {
               Spenden
             </button>
           </Link>
+          <button onClick={toggleMenu} aria-expanded={isOpen} type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"> 
+                <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15" />
+                </svg>
+          </button>
         </div>
-        <div className="items-center justify-between w-full md:flex md:w-auto md:order-1" id="navbar">
+        <div className={`items-center justify-between ${isOpen ? 'block' : 'hidden'} w-full md:flex md:w-auto md:order-1`} id="navbar">
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-white">
             {navbar.map((item) => (
               <li key={item.id}>
