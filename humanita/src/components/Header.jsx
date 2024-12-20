7cb53cbfaf9551b55b8b510b3610437a2fea6b65
Tobias Huppertz <tobias.huppertz@iu-study.org>
2024-12-20 11:32:24 +0100

    Edit Header a-tag to Link

--- humanita/src/components/Header.jsx
+++ humanita/src/components/Header.jsx
@@ -33,9 +33,9 @@ export default function Navbar() {
           <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 md:flex-row md:mt-0 md:border-0 md:bg-white">
             {navbar.map((item) => (
               <li key={item.id}>
-                <a href={item.url} onClick={() => setIsOpen(false)} className="block py-2 px-3 font-SourceSans text-sm text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-500">
+                <Link to={item.url} onClick={() => setIsOpen(false)} className="block py-2 px-3 font-SourceSans text-sm text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-green-500">
                   {item.title}
-                </a>
+                </Link>
               </li>
             ))}
           </ul>
